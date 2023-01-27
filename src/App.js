063f8819a29cd702d3dc0c58daae7f85d7f12b1d
Brandon Knight <knightbrandon1984@gmail.com>
2023-01-27 12:17:46 -0600

    initial setup of folders and files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Kenobi.
-          hello there...
-          there we go......
-        </a>
-      </header>
-    </div>
-  );
+    return <div>App</div>
 }
 
 export default App;

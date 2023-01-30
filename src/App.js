8229059281a9cf70ab0e2d530f8c5c6fbb8642b9
Brandon Knight <knightbrandon1984@gmail.com>
2023-01-30 00:07:04 -0600

    search bar now finds images as desired

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import SearchBar from './components/SearchBar';
+import searchImages from './api'
 
 function App() {
     const handleSubmit = (term) => {
-        console.log('Do a search with', term);
-    }
+    searchImages(term);
+}
 
     return (
     <div>
